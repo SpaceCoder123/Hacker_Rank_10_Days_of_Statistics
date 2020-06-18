3f999dce5c76ca9a1c497a605316b34bc5507e42
Mohan Ram T.R <51279552+SpaceCoder123@users.noreply.github.com>
2020-06-18 19:01:53 +0530

    Update Day1_Quartiles.py

--- Day1_Quartiles.py
+++ Day1_Quartiles.py
@@ -1,7 +1,6 @@
+n = int(input())
+x = sorted(list(map(int, input().split())))
 from statistics import median
-x=int(input())
-X=list(map(int,input().split()))
-X.sort()
-print(round(median(X[:5])))
-print(round(median(X)))
-print(round(median(X[-5:])))
+print(int(median(x[:n//2])))
+print(int(median(x)))
+print(int(median(x[(n+1)//2:])))
